5dddb3514e4577e6ec3deb43b0ae74c711aea5a7
Leandro Cotti <lefcott@hotmail.com>
2022-04-10 03:19:31 -0300

    rotate player to pointer

--- web/src/objects/player.js
+++ web/src/objects/player.js
@@ -10,22 +10,14 @@ class Player extends Phaser.GameObjects.Sprite {
       "player"
     );
     this.scene = scene;
-    this.scale = 0.35;
+    this.scale = 0.32;
     this.scene.add.existing(this);
   }
   checkMovements() {
-    if (this.scene.input.keyboard.addKey("W").isDown) {
-      this.moveUp();
-    }
-    if (this.scene.input.keyboard.addKey("A").isDown) {
-      this.moveLeft();
-    }
-    if (this.scene.input.keyboard.addKey("S").isDown) {
-      this.moveDown();
-    }
-    if (this.scene.input.keyboard.addKey("D").isDown) {
-      this.moveRight();
-    }
+    if (this.scene.input.keyboard.addKey("W").isDown) this.moveUp();
+    if (this.scene.input.keyboard.addKey("A").isDown) this.moveLeft();
+    if (this.scene.input.keyboard.addKey("S").isDown) this.moveDown();
+    if (this.scene.input.keyboard.addKey("D").isDown) this.moveRight();
   }
   moveLeft() {
     this.scene.background.moveRight();
@@ -39,7 +31,18 @@ class Player extends Phaser.GameObjects.Sprite {
   moveDown() {
     this.scene.background.moveUp();
   }
+  rotateToPointer() {
+    const pointer = this.scene.input.activePointer;
+    const angle = Phaser.Math.Angle.Between(
+      this.x,
+      this.y,
+      pointer.x,
+      pointer.y
+    );
+    this.setRotation(angle + Math.PI / 2);
+  }
   update() {
+    this.rotateToPointer();
     this.checkMovements();
   }
 }
