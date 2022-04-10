f6f34d1f298768839992689122559b4306f573ac
Leandro Cotti <lefcott@hotmail.com>
2022-04-10 03:11:28 -0300

    added game borders

--- web/src/objects/background.js
+++ web/src/objects/background.js
@@ -1,22 +1,44 @@
 import MainScene from "..";
 
+const width = 6400;
+const height = 6400;
+
 class Background extends Phaser.GameObjects.TileSprite {
   /** @param {MainScene} scene */
   constructor(scene) {
-    super(scene, 0, 0, 6400, 6400, "background");
+    super(
+      scene,
+      scene.game.canvas.width / 2,
+      scene.game.canvas.height / 2,
+      width,
+      height,
+      "background"
+    );
+    this.minX = scene.game.canvas.width / 2 - width / 2;
+    this.minY = scene.game.canvas.height / 2 - height / 2;
+    this.maxX = scene.game.canvas.width / 2 + width / 2;
+    this.maxY = scene.game.canvas.height / 2 + height / 2;
     scene.add.existing(this);
   }
   moveLeft() {
-    this.x -= 10;
+    if (this.x - 10 >= this.minX) {
+      this.x -= 10;
+    }
   }
   moveRight() {
-    this.x += 10;
+    if (this.x + 10 <= this.maxX) {
+      this.x += 10;
+    }
   }
   moveUp() {
-    this.y -= 10;
+    if (this.y - 10 >= this.minY) {
+      this.y -= 10;
+    }
   }
   moveDown() {
-    this.y += 10;
+    if (this.y + 10 <= this.maxY) {
+      this.y += 10;
+    }
   }
 }
 
